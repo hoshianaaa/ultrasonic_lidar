73c4891651209f2fae6668b2849202c948ee7cdf
hoshianaaa <40942409+hoshianaaa@users.noreply.github.com>
2019-07-18 19:20:13 +0900

    Update servo_only.ino

--- arduino/servo_only.ino
+++ arduino/servo_only.ino
@@ -1,24 +1,31 @@
-#include <Servo.h>
-const int serv_pin = 12;
+// SG-5010
+//  制御パルス:0.5~2.4ms
+//  pwmサイクル:20ms
 
-Servo myServo;
+#include <FlexiTimer2.h>
+
+const int servo = 3;
+int servo_pulse_ = 0;
+
+void flash() {
+  digitalWrite(servo, HIGH);
+  delayMicroseconds(servo_pulse_);
+  digitalWrite(servo, LOW);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  myServo.attach(serv_pin);
   Serial.begin(9600);
+
+  pinMode(servo, OUTPUT);
+  FlexiTimer2::set(20, flash); // 500ms period
+  FlexiTimer2::start();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  // SG-5010
-  //制御パルス:0.5~2.4ms
-  //pwmサイクル:20ms
-  
-  // ServoLibrary:1~2ms->使えない
-  
-  
-  myServo.write(180);
-  
-
+  servo_pulse_ = 500;
+  Serial.println(servo_pulse_);
+  delay(1000);
+  servo_pulse_ = 2400;
+  Serial.println(servo_pulse_);
+  delay(1000);
 }
